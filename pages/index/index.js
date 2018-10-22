c0fec02ecb69436f9a2fb33c6d9b0c35fff8f885
“lihui41@58ganji.com <lihui41@58ganji.com>
2018-10-22 23:39:35 +0800

    数据绑定

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,17 @@
 var param = {
+  data: {
+    message: '微信小程序',
+    array: [1, 2, 3, 4, 5],
+    view: "APP",
+    zhangsan: {name: '张三', age: 18},
+    lisi: {name: '李四', age: 28},
+    count: 0,
+  },
+  add: function(e) {
+    let count2 = this.data.count + 1;
+    this.setData({count: count2})
+    console.log(e);
+  }
 };
 
 Page(param);
