c639cfa0a57c53e15632bd3ff305680ae62b407f
“lihui41@58ganji.com <lihui41@58ganji.com>
2018-10-23 23:23:32 +0800

    判断

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,16 +1,8 @@
 var param = {
   data: {
-    message: '微信小程序',
-    array: [1, 2, 3, 4, 5],
-    view: "APP",
-    zhangsan: {name: '张三', age: 18},
-    lisi: {name: '李四', age: 28},
-    count: 0,
-  },
-  add: function(e) {
-    let count2 = this.data.count + 1;
-    this.setData({count: count2})
-    console.log(e);
+    length : 2,
+    name: 'lihui',
+    isShow: false,
   }
 };
 
